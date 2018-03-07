a21f12eb30a9d9f52b290e6ca92f1be90a42a273
Sachin Palta <sachinpalta@gmail.com>
2018-03-07 12:33:10 -0500

    hello

---
+++ Project 2.0.py
@@ -0,0 +1,12 @@
+import pygame
+(width,height)=(600,500)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+b
+while True:
+    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            running = False
