207b6921292a5a8cbc1f2fff442ebcd32678975b
Kew Forest Teacher <daronson@kewforest.org>
2018-05-14 22:16:12 -0400

    new

--- app/catseverywhere.py
+++ app/catseverywhere.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import ast
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +8,29 @@ def hello_world():
     name = "myself"
     return render_template('index.html', author=author, name=name)
 
+emailfile=open("email.txt","r")
+mystr= emailfile.readline()
+email_addresses=ast.literal_eval(mystr)
+emailfile.close()
+
+
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+    email = request.form['email']
+    email_addresses.append(email)
+    print(email_addresses)
+    mysave= open("email.txt","w")
+    mystr= str(email_addresses)
+    mysave.write(mystr)
+    mysave.close()
+    return redirect('/')
+
+@app.route('/emails.html')
+def emails():
+    return render_template('emails.html', email_addresses=email_addresses)
+
 if __name__ == "__main__":
     app.run()
+
+
